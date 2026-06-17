b641f4b40556a1319bbec29ef2c4e9ba1044578f
Elisha Ebenezer <elisha.ebenezer@eeworx.dev>
2026-06-17 18:00:43 +0530

    check for env in the right place

--- server/scripts/start.bat
+++ server/scripts/start.bat
@@ -17,8 +17,8 @@ if "%BASENAME%"=="scripts" (
 cd /d %SERVER_ROOT%
 
 REM Load env.bat if present
-if exist env.bat (
-  call env.bat
+if exist conf\env.bat (
+  call conf\env.bat
 )
 
 REM Locate JAR
